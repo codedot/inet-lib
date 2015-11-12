e6728061d2d16520435ca5ed3845bc458cb302f9
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2015-11-13 01:02:19 +0200

    Revert "Move obj2mlc() to *.in"
    This reverts commit a2d8d05f35d52e88d796d19561c3c68c76cf3ca6.

--- index.js
+++ index.js
@@ -5,6 +5,44 @@ var inverb, inrules, inconf, inenv, inqueue, nwires, nambs;
 var typelist, types, ntypes, wiretype, ambtype, table;
 var lpaxtype, rpaxtype;
 
+function obj2mlc(obj)
+{
+	var node = obj.node;
+
+	if ("atom" == node)
+		return obj.name;
+
+	if ("abst" == node) {
+		var body = obj2mlc(obj.body);
+		var sep;
+
+		if ("abst" == obj.body.node)
+			sep = ", ";
+		else
+			sep = ": ";
+
+		return obj.var + sep + body;
+	}
+
+	if ("appl" == node) {
+		var left = obj2mlc(obj.left);
+		var right = obj2mlc(obj.right);
+
+		if ("abst" == obj.left.node)
+			left = "(" + left + ")";
+
+		if ("abst" == obj.right.node)
+			right = "(" + right + ")";
+
+		if ("appl" == obj.right.node)
+			right = "(" + right + ")";
+
+		return left + " " + right;
+	}
+
+	return "[ ]";
+}
+
 function addtypes(tree)
 {
 	var node = tree.node;
@@ -628,6 +666,16 @@ function getlist(pax)
 		return "";
 }
 
+function format(data)
+{
+	if ("object" == typeof data)
+		return obj2mlc(data);
+	else if ("number" == typeof data)
+		return data.toString();
+	else
+		return data;
+}
+
 function gettree(agent)
 {
 	var type = agent.type;
@@ -660,12 +708,12 @@ function gettree(agent)
 
 		human = "\\amb#" + index + list;
 	} else {
-		var data = agent.data;
+		var data = format(agent.data);
 
-		if (undefined == data)
-			data = "";
-		else
+		if (data)
 			data = "_{" + data + "}";
+		else
+			data = "";
 
 		type = typelist[type] + data;
 
@@ -754,6 +802,7 @@ function run(mlc)
 	reduce();
 
 	inenv.stats = getstats();
+	inenv.nf = obj2mlc(inenv.nf);
 	return inenv;
 }
 
